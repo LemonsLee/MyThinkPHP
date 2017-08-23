26e331fca83b4043698e2640cb83b9641b35d0a1
lemonslee_hn <lemonslee_hn@sina.com>
2017-08-23 12:40:01 +0800

    Nothing

--- Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
+++ Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
@@ -1,6 +1,11 @@
 <?php if (!defined('THINK_PATH')) exit();?><html>
-<head><title>IndexController index</title></head>
-<body>
-<h1>Hello <em><?php echo ($user['username']); ?></em> !</h1>
-</body>
+    <head>
+	<title>IndexController index</title>
+    </head>
+    <body>
+        <h1>Hello <?php echo ((isset($user['username']) && ($user['username'] !== ""))?($user['username']):"Jerry"); ?></h1>
+	<?php if(($status) == "1"): ?>normal<?php endif; ?>
+	<?php if(is_array($list)): $i = 0; $__LIST__ = $list;if( count($__LIST__)==0 ) : echo "" ;else: foreach($__LIST__ as $key=>$vo): $mod = ($i % 2 );++$i; echo ($vo["username"]); ?>:<?php echo ($vo["email"]); ?>
+	    <br/><?php endforeach; endif; else: echo "" ;endif; ?>
+    </body>
 </html>
